5b99b7e8b9a55e7f030817caaeafb20067cf0de4
Von <mendresvon@gmail.com>
2025-09-20 17:05:07 +0800

    Renamed 01-io -> io

---
+++ io/io_image.py
@@ -0,0 +1,15 @@
+import cv2
+import os
+
+# read image
+image_path = os.path.join('.', 'data', 'bird.jpg')
+
+img = cv2.imread(image_path)
+
+# write image
+cv2.imwrite(os.path.join('.', 'data', 'bird_out.jpg'), img)
+
+# visualize image
+cv2.imshow('image', img)
+cv2.waitKey(5000)
+
---
+++ io/io_video.py
@@ -0,0 +1,20 @@
+import os
+import cv2
+
+# read video
+video_path = os.path.join(".", "data", "galaxy.mp4")
+
+video = cv2.VideoCapture(video_path)
+
+# visualize video
+
+ret = True
+while ret:
+    ret, frame = video.read()
+
+    if ret:
+        cv2.imshow("frame", frame)
+        cv2.waitKey(120)
+
+video.release()
+cv2.destroyAllWindows()
